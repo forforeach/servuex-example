fa9b99a583d614533d138e137c5625d3e0e670d3
forforeach <forforeach@gmail.com>
2020-05-02 16:57:50 +0300

    refactor(*): fix the vies and remove redundant component

---
+++ src/components/Header.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="app-header">
+    <div class="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <div class="user-box">
+      <img class="avatar" alt="Vue logo" src="../assets/logo.png" />
+      <div class="name">John Doe</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.app-header {
+  display: flex;
+  align-items: center;
+  height: 80px;
+  box-shadow: 0 2px 3px -2px rgba(0, 0, 0, 0.5);
+}
+.nav {
+  margin: auto 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    margin: auto 5px;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+.avatar {
+  height: 50px;
+  margin: auto 15px;
+}
+.user-box {
+  display: flex;
+  align-items: center;
+  margin: auto 30px auto auto;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h2>Hello: {{ fullName }}</h2>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
+  name: "Home"
 };
 </script>
+
+<style lang="scss" scoped>
+.home {
+  margin: 10px;
+  text-align: center;
+}
+</style>
